12f81df27aa9e1d653dc8641a890557970a0e679
Aidan Slovinski <83311951+BEQUANTaSlov@users.noreply.github.com>
2021-11-22 15:47:10 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-keys = dict(
-	consumer_key = 'B1dtsLBBTIIf3Vwd7yDR7utxI', #API Key
-	consumer_secret = 'aU1BtFUQyPRoZSWLs33a1dlT1cBkG6fH2PXzjHhz9WkE5PYMqC',
-	access_token = '1415804457321963533-CblHZuvOcPjQ1WZsYMS0qPvxdkb0a2',
-	access_token_secret = 'k73X1Xz5rZ0xuGUanY5G9MEcT7znK59WNOJAyHptakURc',
-)
